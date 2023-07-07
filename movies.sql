e121e1cbfa18ec8c43164bb3a6ec5f950a1e4c82
SuelenGalhardo <suelenranucci@gmail.com>
2023-07-07 10:58:30 +0200

    added create movies

---
+++ movies.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM netflix.movies
+LIMIT 0, 1000
+
+-- Date: 2023-07-07 10:57
+*/
+INSERT INTO `` (`id_Movie`,`title`,`genre`,`image`,`category`,`year`) VALUES (1,'Pulp fiction','Crimen.','https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg','Top 10',1994);
+INSERT INTO `` (`id_Movie`,`title`,`genre`,`image`,`category`,`year`) VALUES (2,'    La vita è bella','Comedia','https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg','Top 10',1994);
+INSERT INTO `` (`id_Movie`,`title`,`genre`,`image`,`category`,`year`) VALUES (3,'Forrest Gump ','Comedia','https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg','Top 10',NULL);
