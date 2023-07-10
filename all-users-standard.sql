c0bb39ae29752e47723567c250edb9391a846d67
raquelgarciat <raquelgarciat@gmail.com>
2023-07-10 11:40:40 +0200

    add queries

---
+++ all-users-standard.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT *
+FROM users
+WHERE plan_details = 'Standard'
+LIMIT 0, 1000
+
+-- Date: 2023-07-10 11:32
+*/
+INSERT INTO `` (`idUsers`,`user`,`password`,`name`,`email`,`plan_details`) VALUES (1,'laura_dev','laura','Laura','laura@gmail.com','Standard');
+INSERT INTO `` (`idUsers`,`user`,`password`,`name`,`email`,`plan_details`) VALUES (2,'maria_dev','maria','Maria','maria@gmail.com','Standard');
+INSERT INTO `` (`idUsers`,`user`,`password`,`name`,`email`,`plan_details`) VALUES (3,'ester_dev','ester','Ester','ester@gmail.com','Standard');
