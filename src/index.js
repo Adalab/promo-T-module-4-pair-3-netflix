46cd7ca676b6880e9fd75766ea0d93b64671354d
MariaPeCa <mpenacantero@gmail.com>
2023-07-14 11:38:05 +0200

    fetch movies DDBB and connect to mysql

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+//connect to DDBB
+
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    database: 'netflix',
+    user: 'root',
+    password: 'pitijopoOjo!',
+  });
+  await connection.connect();
+
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+  );
+
+  return connection;
+}
+server.get('/movies', async (req, res) => {
+  console.log('Pidiendo a la base de datos información de las películas.');
+  let sql = 'SELECT * FROM movies';
+
+  const connection = await getConnection();
+  const [results] = await connection.query(sql);
+  res.json({success:true, movies:results});
+  connection.end();
+});
+
 
 // init express aplication
 const serverPort = 4000;
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,32 +1,15 @@
 // login
 
-const getMoviesFromApi = () => {
+const getMoviesFromApi = async () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
-    .then(() => {
+  
+  const response = await fetch ('http://localhost:4000/movies');
+
+    const dataMoviesJson = await response.json();
+    return dataMoviesJson;
+  
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            genre: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            genre: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
-    });
 };
 
 const objToExport = {
